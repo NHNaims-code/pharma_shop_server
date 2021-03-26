5af93381a1bda59a56b7df4cd5ae8c8b71a0224d
NHNaims-code <mdnimurhasan99@gmail.com>
2021-03-26 22:06:00 +0600

    try to fiexed cors policy error from localhost

--- index.js
+++ index.js
@@ -21,6 +21,22 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+//Extra added in test mood
+app.use(function(req, res, next) {
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type"
+  );
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  next();
+});
+//Extra added in test mood
+
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/result.pdf");
 });
