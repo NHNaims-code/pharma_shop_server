39184f94f0808f7a7a0d915faa064c36cf72fc1c
NHNaims-code <mdnimurhasan99@gmail.com>
2021-04-08 00:32:02 +0600

    fixed mid problem

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const bodyParser = require("body-parser");
 const MongoClient = require("mongodb").MongoClient;
 const ObjectId = require("mongodb").ObjectId;
 const pdf = require("html-pdf");
-const pdfTemplate = require("./documents");
+// const pdfTemplate = require("./documents");
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.uw7zf.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
